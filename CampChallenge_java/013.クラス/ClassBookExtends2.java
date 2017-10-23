733b18bab730a9a06cd2904f8debab823a2f79fb
Akira221 <a.imamura16@gmail.com>
2017-10-23 17:34:12 +0900

    課題「クラス２クラスの継承（extends)」再提出用

---
+++ CampChallenge_java/013.クラス/ClassBookExtends2.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackagesample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+class Book {
+
+    public String title = "";
+    public int money = 0;
+
+    public void setBook(String t, int m) {
+        this.title = t;
+        this.money = m;
+
+    }
+
+    public void print(PrintWriter pw) {
+
+        pw.print(this.title + this.money);
+    }
+
+}
+
+class Booktwo extends Book {
+
+    public void setClear() {
+        this.title = null;
+        this.money = 0;
+
+    }
+}
+    public class ClassBookExtends extends HttpServlet {
+
+        /**
+         * Processes requests for both HTTP <code>GET</code> and
+         * <code>POST</code> methods.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException {
+            response.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = response.getWriter()) {
+                Book m = new Book();
+                Booktwo m2 = new Booktwo();
+
+               // m.setBook("週刊毎日", 100);
+
+                m2.setClear();
+
+                m.print(out);
+                /* TODO output your page here. You may use following sample code. */
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Servlet ClassBookExtends</title>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1></h1>");
+                out.println("</body>");
+                out.println("</html>");
+
+            }
+        }
+    
+        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+        /**
+         * Handles the HTTP <code>GET</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        /**
+         * Handles the HTTP <code>POST</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        /**
+         * Returns a short description of the servlet.
+         *
+         * @return a String containing servlet description
+         */
+        @Override
+        public String getServletInfo() {
+            return "Short description";
+        }
+    }
+// </editor-fold>
+    
+    
