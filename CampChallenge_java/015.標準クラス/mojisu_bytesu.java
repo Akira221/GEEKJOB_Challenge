34ae95b2811e8ced361fc917f8f1d65c1f22d21c
Akira221 <a.imamura16@gmail.com>
2017-10-29 13:48:06 +0900

    標準クラス課題「文字列１_文字数とバイト数の取得」

---
+++ CampChallenge_java/015.標準クラス/mojisu_bytesu.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackagesample;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class mojisu_bytesu {
+    public static void main(String[] args){
+        String name = "今村陽";
+                System.out.println(name.length());
+                System.out.println(name.getBytes().length);
+    }
+    
+}
