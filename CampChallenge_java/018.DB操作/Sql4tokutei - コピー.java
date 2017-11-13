67067080f81108e9afbc6a10c1468a7270b3379d
COMPUTERNAME\user <a.imamura16@gmail.com>
2017-11-13 12:14:24 +0900

    DB操作課題４「特定のレコードの情報取得と表示」（再）

---
+++ CampChallenge_java/018.DB操作/Sql4tokutei - コピー.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SQLtest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ *
+ * @author user
+ */
+public class Sql4tokutei extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            Connection db_con = null;
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db", "root", "");
+
+            PreparedStatement db_st = db_con.prepareStatement("Select * from profiles where profilesID = ? ");
+            db_st.setInt(1, 1);
+            ResultSet rs = db_st.executeQuery();
+             while(rs.next()) {
+                int profilesID = rs.getInt("profilesID");
+                String name = rs.getString("name");
+                String tel = rs.getString("tel");
+                int age = rs.getInt("age");
+                String birthday = rs.getString("birthday");
+                out.println("<p>");
+                out.println("プロフィールID:" + profilesID + ", 名前:" + name
+                        + ", 電話:" + tel + ",年齢:" + age + ",誕生日:" + birthday + "<br>");
+                out.println("</p>");
+            }
+rs.close();
+db_con.close();
+db_st.close();
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Sql4tokutei</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Sql4tokutei at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (Exception ex) {
+            System.out.print(ex.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
